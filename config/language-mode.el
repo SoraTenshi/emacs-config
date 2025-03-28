4f9128e38620a65e8e0f0fab75b0249160b0baa0
SoraTenshi <dream@neoncity.dev>
2025-03-28 03:01:02 +0100

    update

--- config/language-mode.el
+++ config/language-mode.el
@@ -1,7 +1,7 @@
-;;; language-mode.el ---
+;;; language-mode.el --- Language specific settings -*- lexical-binding: t; -*-
+;;; Commentary:
 ;; Just loads all the language files
 ;;; Code:
-
 (load-config "config/language/c.el")
 (load-config "config/language/common-lisp.el")
 (load-config "config/language/elixir.el")
@@ -12,5 +12,15 @@
 (load-config "config/language/scheme.el")
 (load-config "config/language/zig.el")
 (load-config "config/language/nix.el")
+
+(defmacro make-compile-command (lang command)
+  "Create hook for LANG to create the compile command COMMAND."
+  `(add-hook ',(intern (concat (symbol-name lang) "-mode-hook"))
+             (lambda ()
+               (set (make-local-variable 'compile-command) ,command))))
+
+(make-compile-command rust "cargo build")
+(make-compile-command go "go build")
+(make-compile-command zig "zig build")
 
 ;;; language-mode.el ends here
