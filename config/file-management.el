8685231c82e0bdd20b9c5c115b3cfa8286f44b2e
SoraTenshi <dream@neoncity.dev>
2025-07-20 23:25:42 +0200

    update

--- config/file-management.el
+++ config/file-management.el
@@ -12,7 +12,7 @@
   :custom
   (completion-styles '(orderless basic))
   (completion-category-defaults nil)
-  (completion-category-overrides '((file (styles partial-completion)))))
+  (completion-category-overrides '((file (styles basic partial-completion)))))
 
 (use-package marginalia
   :straight t
@@ -22,5 +22,10 @@
   :straight t
   :init (setq consult-ripgrep-args
               "rg --null --line-buffered --color=never --max-columns=1000 --path-separator /   --smart-case --no-heading --with-filename --line-number --search-zip --no-ignore-dot --no-require-git"))
+
+(defun nav/global-search (dir)
+  "Search a DIR for a matching regex."
+  (interactive "DDirectory: ")
+  (consult-ripgrep dir))
 
 ;;; file-management.el ends here
--- config/project.el
+++ config/project.el
@@ -3,49 +3,10 @@
 ;; Project setup or something
 ;;; Code:
 
-(setq project-search-path '(("~/dev" . 2)
-                            ("~/source/repos" . 2)))
+(project-remember-projects-under "~/source/repos" nil)
+(project-remember-projects-under "~/dev" nil)
 
 ;; Sets up jj to also be included
 (setq project-vc-extra-root-markers '(".jj"))
-
-(defun project--root-markers ()
-  "Append root markers to already existing markers."
-  (append (when (boundp 'project-vc-extra-root-markers)
-            project-vc-extra-root-markers)
-          '(".git" ".hg" ".bzr" ".svn")))
-
-(defun project--is-root-dir-p (dir)
-  "Test whether this is the start of a root DIR."
-  (cl-some (lambda (m)
-             (file-exists-p (expand-file-name m dir)))
-           (project--root-markers)))
-
-(defun project--discover-roots ()
-  "Discover all possible project roots."
-  (let (roots)
-    (cl-labels ((scan (dir depth)
-                  (when (and (file-directory-p dir)
-                             (not (member (file-name-nondirectory dir)
-                                          '("." ".."))))
-                    (if (project--is-root-dir-p dir)
-                        (push dir roots)
-                      (when (> depth 0)
-                        (dolist (sub (directory-files dir t "^[^.]" t))
-                          (scan sub (1- depth))))))))
-      (dolist (entry project-search-path)
-        (scan (expand-file-name (car entry)) (cdr entry)))
-      (delete-dups roots))))
-
-(defun project/switch ()
-  "Select between all known project paths."
-  (interactive)
-  (let* ((scanned (project--discover-roots))
-         (known (project-known-project-roots))
-         (all (delete-dups (append known scanned)))
-         (choice (completing-read
-                  "Select project: " all nil t nil nil nil)))
-    (when (and choice (not (string-empty-p choice)))
-      (dired choice))))
 
 ;;; project.el ends here
