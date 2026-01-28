397b9fbd425d4fb9c078bda603a1ca2270ef885e
SoraTenshi <dream@neoncity.dev>
2026-01-28 21:41:12 +0100

    irc better

--- config/irc.el
+++ config/irc.el
@@ -11,13 +11,16 @@
 
 (setq erc-prompt
       (lambda ()
-        (format "[%s:%s]>"
-                (erc-network-name)
-                (or (erc-default-target) ""))))
+        (format "[%s] [%s] <%s> "
+                (format-time-string "%H:%M")
+                (buffer-name)
+                (erc-current-nick)))
+      erc-insert-timestamp-function #'erc-insert-timestamp-left)
 
 (add-hook 'erc-mode-hook
           (lambda ()
             (setq-local buffer-display-table nil)
-            (whitespace-mode -1)))
+            (whitespace-mode -1)
+            (show-paren-mode -1)))
 
 ;;; irc.el ends here
