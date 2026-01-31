78f3c7705376972eaf1a5d52b90e8490f0ad657d
SoraTenshi <dream@neoncity.dev>
2026-01-31 22:02:19 +0100

    theme stuff?

--- emacs-custom-file.el
+++ emacs-custom-file.el
@@ -5,9 +5,15 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
+<<<<<<< Updated upstream
    '("ac4208509154f9617b75b2f0745d1bc1a16d0fac4231097511b328ac87cfcb83"
      "bdc966cf24e51adacf91298922ed981e67309a187887bb8279edcd7236859b1f"
      "8f450bedc8f9a2ec269c6b8debc84d9621bcf9ac258b3bdf3030cab5eab05efa"
+=======
+   '("541b529bb370837d2531c55fabcb5762ec1f7cbade2a1738d539012be5b62dec"
+     "e4b078cd88a40db28ee0b03b5961eb668d53707c75a0f4156ea5289649c20247"
+     "90b3fd39689e48ec9ac7862deb0fdbeeccaf2ffe2999b8078a37d5e5047e28a1"
+>>>>>>> Stashed changes
      default)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
