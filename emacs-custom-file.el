b19520c39fbaab6eb5ccabda884325a6522c8a20
SoraTenshi <dream@neoncity.dev>
2026-02-03 23:43:40 +0100

    Theme update to support magit

--- emacs-custom-file.el
+++ emacs-custom-file.el
@@ -5,7 +5,8 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-safe-themes
-   '("7711e2d4ee53c345cfc6ecd1c8d829988b0508b10ed951e0a369519c2ab3d7d0"
+   '("e6d7f85dd581553bf73c8e36f8dee9d330924a7d1f3a26996e35d767dbcfca1a"
+     "7711e2d4ee53c345cfc6ecd1c8d829988b0508b10ed951e0a369519c2ab3d7d0"
      default))
  '(package-selected-packages
    '(aggressive-indent avy cape colorful-mode consult-eglot corfu
