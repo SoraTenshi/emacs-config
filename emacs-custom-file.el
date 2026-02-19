1a26cd8a17b91de3ec842cde52b195abe213d214
SoraTenshi <dream@neoncity.dev>
2026-02-19 01:06:50 +0100

    ace-window. Kinda neat for buffer mgmt.

--- emacs-custom-file.el
+++ emacs-custom-file.el
@@ -5,16 +5,14 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(package-selected-packages
-   '(aggressive-indent avy cape colorful-mode consult-eglot
-                       consult-flycheck corfu devdocs diminish docker
-                       dockerfile-mode embark-consult erc-image
-                       fancy-compilation flycheck-eglot format-all
-                       gcmh go-mode hel helpful
-                       highlight-indent-guides kind-icon magit
-                       marginalia nix-mode odin-mode orderless
-                       org-modern org-roam paredit pcre2el plz
-                       rust-mode simpc-mode vdiff vertico vundo
-                       zig-mode)))
+   '(ace-window aggressive-indent avy cape colorful-mode consult-eglot
+                consult-flycheck corfu devdocs diminish docker
+                dockerfile-mode embark-consult erc-image
+                fancy-compilation flycheck-eglot format-all gcmh
+                go-mode hel helpful highlight-indent-guides kind-icon
+                magit marginalia nix-mode odin-mode orderless
+                org-modern org-roam paredit pcre2el plz rust-mode
+                simpc-mode vdiff vertico vundo zig-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
